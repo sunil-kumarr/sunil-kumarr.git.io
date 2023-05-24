b6ff5c71b5458159dd4511aba5c8a637baf723d7
sunil-kumarr <sunindus.sk@gmail.com>
2023-05-24 15:46:53 +0530

    add footer

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -4,7 +4,10 @@ import ShadowButton from "../components/ShadowButton";
 import ShadowCard from "../components/ShadowCard";
 import SeverSVG from "../assets/svg/server-diagram.svg";
 import Timeline from "../components/Timeline";
-import Skills from "../components/Skills";
+import Book1 from "../assets/img/book1.jpeg";
+import Book2 from "../assets/img/book2.jpeg";
+import Tech1 from "../assets/img/tech2.jpg";
+import Tech2 from "../assets/img/hacka.jpg";
 import { DiJava, DiDocker } from "react-icons/di";
 import {
   SiKubernetes,
@@ -22,7 +25,7 @@ import {
 import { TbApi } from "react-icons/tb";
 
 const HomePage = () => (
-  <div>
+  <main>
     <section class="grid grid-cols-10 grid-rows-10 grid-flow-row">
       <div
         className="col-span-3 h-screen relative"
@@ -76,6 +79,7 @@ const HomePage = () => (
         </div>
       </div>
     </section>
+
     <section class="col-span-10">
       <div
         className="justify-center align-center flex p-10"
@@ -85,15 +89,7 @@ const HomePage = () => (
         <Timeline />
       </div>
     </section>
-    {/* <section class="col-span-10">
-      <div
-        className="justify-center align-center flex p-10"
-        style={{ backgroundColor: "#E3F4F4" }}
-      >
-        <img src={SkillImg} height={200} width={400} />
-        <Skills />
-      </div>
-    </section> */}
+
     <section>
       <div
         className="justify-center align-center flex flex-col p-10"
@@ -179,7 +175,86 @@ const HomePage = () => (
         </div>
       </div>
     </section>
-  </div>
+
+    <section>
+      <div
+        className="justify-center align-center flex flex-col p-10"
+        style={{ backgroundColor: "#E3F4F4" }}
+      >
+        <h1 className=" m-20 customFont"> Other Stuff </h1>
+        <div className="grid grid-cols-3 grid-flow-row">
+          <div
+            className="shadowbox flex flex-col align-center"
+            style={{ width: "fit-content" }}
+          >
+            <div className="flex flex-row align-center border border-black">
+              <div class="relative max-w-sm overflow-hidden bg-cover bg-no-repeat">
+                <img
+                  src={Book1}
+                  class="max-w-xs transition duration-300 ease-in-out hover:scale-110"
+                  alt="Tech Talk"
+                />
+              </div>
+            </div>
+            <div>
+              <p className="p-4">
+                <a
+                  className="no-underline hover:underline"
+                  href="https://www.packtpub.com/product/a-blueprint-for-production-ready-web-applications/9781803248509"
+                  target="_blank"
+                >
+                  Quart Async Framework | Book Reviewer
+                </a>{" "}
+              </p>
+            </div>
+          </div>
+          <div
+            className="shadowbox flex flex-col align-center"
+            style={{ width: "fit-content" }}
+          >
+            <div className="flex flex-row align-center border border-black">
+              <div class="relative max-w-sm overflow-hidden bg-cover bg-no-repeat">
+                <img
+                  src={Tech1}
+                  class="max-w-xl transition duration-300 ease-in-out hover:scale-150"
+                  alt="Tech Talk"
+                />
+              </div>
+            </div>
+            <div>
+              <p className="p-4">
+                <a
+                  className="no-underline hover:underline"
+                  href="https://docs.google.com/presentation/d/10zHL72s9JVvrioXTJWKVb7N-P_k6mPSUtVIoGeLbfUU/edit?usp=sharing
+"
+                  target="_blank"
+                >
+                  Tech Talk On CDC using Kafka by @ME
+                </a>
+              </p>
+            </div>
+          </div>
+          <div
+            className="shadowbox flex flex-col align-center"
+            style={{ width: "fit-content" }}
+          >
+            <div className="flex flex-row align-center border border-black">
+              <div class="relative max-w-sm overflow-hidden bg-cover bg-no-repeat">
+                <img
+                  src={Tech2}
+                  class="max-w-lg transition duration-300 ease-in-out hover:scale-150"
+                  alt="hackathon"
+                />
+              </div>
+            </div>
+            <div>
+              <p className="p-4">Credgenics Hackathon</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  </main>
 );
 
 export default HomePage;
