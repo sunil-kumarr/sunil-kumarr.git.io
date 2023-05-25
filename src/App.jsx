c721356e22ff9de3dedcd77392e2e4a061974be2
sunil-kumarr <sunindus.sk@gmail.com>
2023-05-25 10:42:06 +0530

    head tag

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,4 @@
 import React from "react";
-import BrowserCard from "./components/BrowserCard";
-import ShadowCard from "./components/ShadowCard";
-import BlogCard from "./components/BlogCard";
 import HomePage from "./pages/HomePage";
 import Navbar from "./components/Navbar";
 
