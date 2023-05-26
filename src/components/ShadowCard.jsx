e3b8a167da74011108292cbda990a8aaed17b7f0
sunil-kumarr <sunindus.sk@gmail.com>
2023-05-26 12:57:57 +0530

    add more projects

--- src/components/ShadowCard.jsx
+++ src/components/ShadowCard.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { IconContext } from "react-icons";
 
 const ShadowCard = ({ icons, title, description, date }) => (
-  <div className="shadowbox flex flex-col  align-center">
+  <div className="card shadowbox flex flex-col  align-center">
     <div className="bg-white h-50 grid grid-cols-4 grid-rows-2 grid-flow-row p-10 gap-y-8 gap-x-4">
       <IconContext.Provider value={{ size: "50px" }}>
         {icons.map((Icon, index) => (
--- src/pages/home/sections/ProjectSection.jsx
+++ src/pages/home/sections/ProjectSection.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import ShadowCard from "../../../components/ShadowCard";
 import { DiJava, DiDocker } from "react-icons/di";
+import { MdKeyboardDoubleArrowRight } from "react-icons/md";
 import { TbApi } from "react-icons/tb";
 import {
   SiKubernetes,
@@ -14,34 +15,139 @@ import {
   SiPostgresql,
   SiMongodb,
   SiAndroidstudio,
+  SiAmazonec2,
+  SiAmazons3,
+  SiRedis,
 } from "react-icons/si";
 
 const ProjectSection = () => (
   <section>
     <div className="flex flex-col justify-center align-center p-10 bg-[#FBFFDC]">
       <h1 className="m-20 customFont"> Projects </h1>
-      <div className="grid grid-cols-1 grid-rows-3 grid-flow-col gap-4 lg:grid-cols-3 lg:grid-rows-1 lg:grid-flow-row">
+      <div className="grid grid-cols-1 grid-rows-6 grid-flow-col gap-8 lg:grid-cols-3 lg:grid-rows-1 lg:grid-flow-row">
         <ShadowCard
           icons={[
+            <SiApachekafka />,
             <DiJava />,
             <SiSpringboot />,
-            <SiApachekafka />,
             <SiKubernetes />,
             <DiDocker />,
             <SiGradle />,
             <SiMongodb />,
             <SiPostgresql />,
           ]}
-          date="Nov, 2022 - May, 2023"
+          date="2023"
           title="Workflow Automation"
           description={
             <p>
-              Automation of entire platform using a UI. Where company features
-              are available as action blocks of graph to the user.
-              <ol className="desc_style">
+              Designed and developed backend for a workflow creation tool for
+              automating different platform actions using DAG's and User
+              Interface. Highly customizable and modular design for addition of
+              new actions in less than hour.
+              <ol className="desc_style pt-2">
                 <li>
                   <strong>Tech:</strong> Java, SpringBoot, Apache Kafka, Netflix
-                  Conductor, K8s, Docker, Gradle
+                  Conductor, K8s, Docker, Gradle, Microservices
+                </li>
+              </ol>
+            </p>
+          }
+        />
+
+        <ShadowCard
+          icons={[
+            <SiApachekafka />,
+            <SiPython />,
+            <SiFastapi />,
+            <TbApi />,
+            <SiPostgresql />,
+            <DiDocker />,
+            <SiKubernetes />,
+          ]}
+          date="2022"
+          title="Webhook Data Ingestion Pipeline"
+          description={
+            <p>
+              Designed and developed a webhook ingestion pipeline to process and
+              store webhooks in Postgresql using kafka as buffer and kafka
+              consumer for batching and chaining multiple queries to reduce load
+              on database.
+              <ol className="desc_style pt-2">
+                <li>
+                  <strong>Tech:</strong> Kafka, Python, FastAPI, Postgresql,
+                  Microservice, Docker, Kubernetes
+                </li>
+              </ol>
+            </p>
+          }
+        />
+
+        <ShadowCard
+          icons={[
+            <SiApachekafka />,
+            <SiPostgresql />,
+            <SiAmazonec2 />,
+            <SiAmazons3 />,
+            <SiRedis />,
+            <SiMongodb />,
+          ]}
+          date="2022"
+          title="Change Data Capture Pipeline"
+          description={
+            <p>
+              Configured and Setup entire CDC pipeline components for capturing
+              changes in data from multiple sources into kafka. This pipeline
+              now powers use cases like table auditing, cumulative dashboard and
+              reports, etc...
+              <ol className="desc_style pt-2">
+                <li>
+                  <strong>Tech:</strong> Kafka, Kafka Connect, Debezium,
+                  Postgresql, MongoDB
+                </li>
+              </ol>
+            </p>
+          }
+        />
+        <ShadowCard
+          icons={[
+            <SiPython />,
+            <SiFastapi />,
+            <SiPostgresql />,
+            <DiDocker />,
+            <SiKubernetes />,
+          ]}
+          date="2022"
+          title="Communication Microservice"
+          description={
+            <p>
+              Redesigned and optimised entire service data flow for performance
+              improvement gains in API calls of upto 10x times.
+              <ol className="desc_style pt-2">
+                <li>
+                  <strong>Tech:</strong> Python,FastAPI, Postgresql,
+                  Microservices
+                </li>
+              </ol>
+            </p>
+          }
+        />
+        <ShadowCard
+          icons={[
+            <SiJavascript />,
+            <SiReact />,
+            <DiJava />,
+            <SiAndroidstudio />,
+          ]}
+          date="2021"
+          title="CG Collect (Feet On Street) "
+          description={
+            <p>
+              On Field Collection application for agent to track and recovery
+              collections amount from defaulters.
+              <ol className="desc_style">
+                <li>
+                  <strong>Tech:</strong> Javascript, ReactJs, React Native,
+                  Android, Java
                 </li>
               </ol>
             </p>
@@ -58,8 +164,8 @@ const ProjectSection = () => (
             <DiDocker />,
             <SiKubernetes />,
           ]}
-          date="April, 2021 - Oct, 2021"
-          title="Telecalling Service"
+          date="2021"
+          title="Telecalling Microservice"
           description={
             <p>
               Wrapper Service for vendor API integrations. Integrated more than
@@ -74,29 +180,10 @@ const ProjectSection = () => (
             </p>
           }
         />
-        <ShadowCard
-          icons={[
-            <SiJavascript />,
-            <SiReact />,
-            <DiJava />,
-            <SiAndroidstudio />,
-          ]}
-          date="May, 2021 - Sept, 2021"
-          title="CG Collect (Feet On Street) "
-          description={
-            <p>
-              On Field Collection application for agent to track and recovery
-              collections amount from defaulters.
-              <ol className="desc_style">
-                <li>
-                  <strong>Tech:</strong> Javascript, ReactJs, React Native,
-                  Android, Java
-                </li>
-              </ol>
-            </p>
-          }
-        />
       </div>
+      <button className="shadowbox m-10 w-fit px-10 py-2 self-center">
+        Explore More <MdKeyboardDoubleArrowRight className="inline" />
+      </button>
     </div>
   </section>
 );
