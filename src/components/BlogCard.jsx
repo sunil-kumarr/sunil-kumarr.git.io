4321213077267f6987dac047ee70809a21b04625
sunil-kumarr <sunindus.sk@gmail.com>
2023-05-27 14:56:16 +0530

    add new blog

--- src/components/BlogCard.jsx
+++ src/components/BlogCard.jsx
@@ -1,14 +1,34 @@
 import React from "react";
+import Draggable from "react-draggable";
 
-const BlogCard = () => (
-  <div className="blogbox shadowbox">
-    <div className=" h-4 "></div>
-    <div>
-      <span> 24 Novemember</span>
-      <div className="h-1 w-1 border-black"></div>
-    </div>
-    <p> Hellow here</p>
+const Dots = () => (
+  <div className="px-4 py-2">
+    <span class="dot"></span>
+    <span class="dot"></span>
+    <span class="dot"></span>
   </div>
+);
+
+const BlogCard = ({ title, content, link, tags }) => (
+  <Draggable>
+    <div className=" border border-2 border-[#404040] bg-white rounded-3xl min-w-min max-w-md bottom-200 hover:cursor-grab">
+      <Dots />
+
+      <div className="bg-black h-0.5 rounded-r-sm" />
+      <div className="p-4">
+        <h1 className="text-lg lg:text-2xl font-bold">{title}</h1>
+        <p className="text-sm lg:text-lg pt-2 ">{content}</p>
+        <div className="flex justify-between mt-4 as">
+          <span>{tags}</span>
+          {link && (
+            <a href={link} target="_blank" className="clickLink">
+              Read post →
+            </a>
+          )}
+        </div>
+      </div>
+    </div>
+  </Draggable>
 );
 
 export default BlogCard;
--- src/pages/home/sections/BlogSection.jsx
+++ src/pages/home/sections/BlogSection.jsx
@@ -1,17 +1,25 @@
 import React from "react";
-import BrowserCard from "../../../components/BrowserCard";
+import BlogCard from "../../../components/BlogCard";
 
 const BlogSection = () => (
   <section>
-    <div className="flex flex-col justify-center align-center p-10 bg-[#ae8fdb] max-h-fit lg:flex-row lg:justify-between">
+    <div className="flex flex-col justify-center align-center p-10 bg-[#ae8fdb] max-h-fit  lg:justify-between">
       <h1 className="customFont m-10 lg:m-20 "> Bloogs </h1>
-      <div className="grid grid-cols-1 grid-rows-1 grid-flow-row lg:relative p-4">
-        <BrowserCard
+      <div className="grid grid-cols-1 grid-rows-2 grid-flow-col lg:grid-flow-row lg:grid-cols-2 lg:grid-rows-1 gap-4 m-4">
+        <BlogCard
           title="Creating Custom Middleware"
           content="Today we will create a custom middleware for logging. we all have applications that need logging for multiple purposes like debugging, errors, etc…"
-          tags="ASGI"
+          tags="ASGI | FastAPI"
           link={
             "https://medium.com/@sunindus.sk/creating-custom-middleware-6b7ec5a8edd4"
+          }
+        />
+        <BlogCard
+          title="Change Data Capture: Unleashing the Power of Real-Time Data Integration"
+          content="Change Data Capture, commonly referred to as CDC, is a method for capturing and delivering individual data changes in real-time."
+          tags="CDC | Kafka"
+          link={
+            "https://sunil-kumarr.medium.com/change-data-capture-unleashing-the-power-of-real-time-data-integration-a0c7ad400be6"
           }
         />
       </div>
