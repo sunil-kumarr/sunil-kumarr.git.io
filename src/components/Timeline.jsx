5fdf0cbf484e6ee2a09247007fb9a72f3ae3b015
Harsh <harsh.saroha@myparkplus.com>
2024-02-16 20:11:21 +0530

    update worktimeline

--- src/components/Timeline.jsx
+++ src/components/Timeline.jsx
@@ -1,5 +1,9 @@
 import React from "react";
-
+import ParkplusIcon from "../assets/img/parkplus-icon.png";
+import CregenicsIcon from "../assets/img/credgenics-icon.png";
+import PlaystoreIcon from "../assets/img/playstore.png";
+import JuspayIcon from "../assets/img/juspay-icon.png";
+import RetailpluseIcon from "../assets/img/retailpluse-icon.png";
 const MarkerSVG = () => (
   <svg
     aria-hidden="true"
@@ -16,7 +20,6 @@ const MarkerSVG = () => (
   </svg>
 );
 
-
 const BoltSVG = () => (
   <svg
     xmlns="http://www.w3.org/2000/svg"
@@ -30,10 +33,10 @@ const BoltSVG = () => (
 
 const TimelineEvent = ({ icon, title, time, description, position, pLink }) => (
   <li className={`mb-10 ml-6 ${position}`}>
-    <span class="absolute flex items-center justify-center w-6 h-6 bg-blue-100 rounded-full -left-3 ring-8 ring-white dark:ring-gray-900 dark:bg-blue-900">
+    <span class="absolute flex items-center justify-center w-6 h-6 bg-blue-100 rounded-full -left-3 ring-8 ring-white  ">
       {icon}
     </span>
-    <h3 class="flex items-center mb-1 text-lg font-semibold text-gray-900 dark:text-white">
+    <h3 class="flex items-center mb-1 text-lg font-semibold text-gray-900 ">
       {title}
     </h3>
     <time class="block mb-2 text-sm font-normal leading-none text-gray-400 dark:text-gray-500">
@@ -46,10 +49,10 @@ const TimelineEvent = ({ icon, title, time, description, position, pLink }) => (
       <a
         target="_blank"
         href={pLink}
-        class="inline-flex items-center px-4 py-2 text-sm font-medium text-gray-900 bg-white border border-gray-200 rounded-lg hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:outline-none focus:ring-gray-200 focus:text-blue-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700 dark:focus:ring-gray-700"
+        class="inline-flex items-center px-4 py-2 text-sm font-medium text-gray-900 bg-white border border-gray-200 rounded-lg hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:outline-none focus:ring-gray-200 focus:text-blue-700 "
       >
-        {/* <img src="..assets/img/google-play.png" /> */}
-        Playstore
+        <img src={PlaystoreIcon} className="mr-2 w-4 h-4" />
+        Download From Playstore
       </a>
     )}
   </li>
@@ -58,35 +61,38 @@ const TimelineEvent = ({ icon, title, time, description, position, pLink }) => (
 const Timeline = () => (
   <ol class="relative border-l border-gray-400 dark:border-gray-700 m-4">
     <TimelineEvent
+      title={"Park+ | Senior Software Developer"}
+      icon={<img src={ParkplusIcon} />}
+      time={"July 2023 - Current"}
+      pLink={
+        "https://play.google.com/store/apps/details?id=com.ovunque.parkwheels&pcampaignid=web_share"
+      }
+    />
+    <TimelineEvent
       title={"Credgenics | Senior Software Developer"}
-      icon={<BoltSVG />}
-      time={"Promoted 3rd April, 2023 - Current"}
+      icon={<img src={CregenicsIcon} />}
+      time={"April 2023 - June 2023"}
     />
     <TimelineEvent
       title={"Credgenics | Software Developer"}
-      icon={<MarkerSVG />}
-      time={"1st April, 2021 - 3rd April, 2023"}
+      icon={<img src={CregenicsIcon} />}
+      time={"Jan 2021 - April 2023"}
       pLink={
         "https://play.google.com/store/apps/details?id=com.credgenics.fos&hl=en_US&gl=US&pli=1"
       }
     />
     <TimelineEvent
-      title={"Credgenics | Tech Intern"}
-      icon={<MarkerSVG />}
-      time={"Joined 8th March, 2021 - 1st April, 2021"}
-    />
-    <TimelineEvent
       title={"Retail Pulse Technologies | Software developer"}
-      icon={<MarkerSVG />}
-      time={"1st June, 2020 - Dec, 2020"}
+      icon={<img src={RetailpluseIcon} />}
+      time={"June 2020 - December 2020"}
       pLink={
         "https://play.google.com/store/apps/details?id=ai.retailpulse&hl=en_US&gl=US"
       }
     />
     <TimelineEvent
       title={"Juspay Technologies Pvt Ltd | Software Intern"}
-      icon={<MarkerSVG />}
-      time={"April, 2019 - Sept, 2019"}
+      icon={<img src={JuspayIcon} />}
+      time={"April 2019 - Sept 2019"}
     />
   </ol>
 );
